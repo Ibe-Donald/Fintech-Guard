ad68197b21320b30ca0a1fc891c7b26f3508d414
Chibuike Ibe <chibuikeibe.ic@gmail.com>
2026-03-26 13:17:19 +0100

    Initial commit

---
+++ src/main/java/com/chibuike/fintechguard/FintechGuardApplication.java
@@ -0,0 +1,13 @@
+package com.chibuike.fintechguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintechGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chibuike/fintechguard/FintechGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chibuike.fintechguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
